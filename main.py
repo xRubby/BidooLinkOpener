0e0698cbb6478b44b7ba4b21e1cca0e3f1f6408d
Ruben Gigante <rubengigante7@gmail.com>
2025-01-17 13:08:52 +0100

    Aggiunta verifica Login

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 
-class BidooLogin():
+class Bidoo():
     def setup_method(self, method):
-        self.driver = webdriver.Edge()  # Assicurati che EdgeDriver sia nel PATH
+        self.driver = webdriver.Edge()
         self.vars = {}
 
     def bidooLogin(self):
@@ -47,6 +47,19 @@ class BidooLogin():
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".btlogin:nth-child(1)"))
         ).click()
+
+    def is_logged_in(self):
+        """Verifica se l'utente è loggato controllando la presenza di un elemento visibile solo dopo il login"""
+        try:
+            # Aspetta che l'elemento che indica il login avvenuto sia visibile (può essere modificato in base al sito)
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, 'divSaldoBidMobile'))
+            )
+            print("Utente loggato")
+            return True  # Login riuscito
+        except Exception as e:
+            print(f"Errore durante la verifica del login: Utente non loggato")
+            return False  # Login fallito
 
     def extract_links_from_file(self, filename):
         """Estrai solo i link dal file di testo, ignorando il testo non URL."""
@@ -78,14 +91,20 @@ class BidooLogin():
             time.sleep(2 + (time.time() % 1))  # Pausa casuale tra 2 e 3 secondi
 
     def teardown_method(self, method):
-        input("Premi Enter per chiudere il browser...")  # Aggiungi questa riga per mettere una pausa
+        input("Premi Enter per chiudere il browser...")
         self.driver.quit()
 
 if __name__ == "__main__":
-    # Esegui il test
-    bidoo = BidooLogin()
+    # Esegui il login
+    bidoo = Bidoo()
     bidoo.setup_method(None)
     bidoo.bidooLogin()
+    if(not bidoo.is_logged_in()):
+        bidoo.teardown_method(None)
+        exit()
+
+    #Pausa di 5 secondi
+    time.sleep(5)
 
     # Estrai e apri i link
     links = bidoo.extract_links_from_file('links.txt')
